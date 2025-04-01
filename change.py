30dd4fb6673c944d55142dbc1a31eb3cc6df45b8
tomasmontesano1 <timontesano@mail.austral.edu.ar>
2025-04-01 16:43:05 -0300

    change.py

--- change.py
+++ change.py
@@ -1,3 +1,18 @@
 def change():
     expense = 23.75
     money = 100
+    c1=int(money-expense)
+    c2=money-expense
+    c3=c2-c1
+    c3=int(c3*100)   
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(c1)
+    print("Centavos:")
+    
