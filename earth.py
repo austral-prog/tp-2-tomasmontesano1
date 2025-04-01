e2cfc3936fa308d544a7ba1e3b665af92e8fbf4d
tomasmontesano1 <timontesano@mail.austral.edu.ar>
2025-04-01 16:18:39 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,11 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    v1= x > y
+    v2= x < y
+    v1= str(v1)
+    v2= str(v2)
+    print(f"The result of " + {x} + " comes first in the dictionary than " + {y} + " is " + {v1} +".")
+    print(f"The result of " + {y} + " comes first in the dictionary than " + {x} + " is " + {v2} + ".")
+
+    
